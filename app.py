933c548822a3a83bd4f079cd8fd590885a71dad2
lunixbr <lunixbr@gmail.com>
2021-04-09 00:05:15 +0000

    CSRF

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-# Versão 1.0
+# Versão 2.0
 #print("Hello World")
 
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)
+
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run(debug=True)
