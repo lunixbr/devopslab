a42078dc3294f93e5460cc11cdfd9141c78beaea
lunixbr <lunixbr@gmail.com>
2021-04-09 01:07:14 +0000

    Bug Removido

--- app.py
+++ app.py
@@ -15,15 +15,3 @@ def pagina_inicial():
 if __name__ == '__main__':
     app.run(debug=True)
 
-#@app.route('/bug')                                                                                                                                
-#def bad():                                                                                                                                        
-#    try:                                                                                                                                          
-#        raise TypeError()                                                                                                                         
-#    except TypeError as e:                                                                                                                        
-#        print(e)                                                                                                                                  
-#    except TypeError as e:                                                                                                                        
-#        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-##
-#
-#if __name__ == '__main__':
-#    app.run(debug=True)
